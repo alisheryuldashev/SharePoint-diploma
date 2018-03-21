c414067ce1e5a25a0fca4e90d429e571c3b0a470
Alisher Yuldashev <alyuldashev@yahoo.ca>
2018-03-20 23:42:26 -0600

    Update exercise2.cs

--- Server Side Development/C Sharp/Control Flow/exercise2.cs
+++ Server Side Development/C Sharp/Control Flow/exercise2.cs
@@ -3,19 +3,12 @@
 
  
         #region explanation of the algorithm that I used
-        /*input = int;
-        1. figure out start position
-            a)  y = 7; x = 0; 
-        2. write y number of stars *
-        3. set new position 
-            a) y = y - 1; x = x + 1;
-        4. repeat 2 and 3 till x = input or y = 0; 
-        */
-        #endregion
-     
-        /*
+                /*
 			access to this content is restricted for privacy reasons 
 			if you are a prospective employer and would like to see my source code,
 			please contact me for more information. 
 		*/
+        #endregion
+     
+
 		    
