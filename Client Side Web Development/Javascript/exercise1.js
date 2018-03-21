5b34d08b686ef07fa7b7539998b5bb803617f1fc
Alisher Yuldashev <alyuldashev@yahoo.ca>
2018-03-20 23:24:15 -0600

    Update exercise1.js

--- Client Side Web Development/Javascript/exercise1.js
+++ Client Side Web Development/Javascript/exercise1.js
@@ -1,32 +1,8 @@
 //JavaScript Lab 4 Exercise 1 by Alisher Yuldashev. 
 //Programming Diploma - SharePoint Specialization. Innotech College
 
-'use strict';	// this turns on strict for all the JavaScript code on the web page
-
-
-// This function is called by the Input Button when the user clicks the button
-function calculate()
-{
-	var pi = 3.1415926535897932;
-	var radius = 0;
-	var volume = 0;
-	
-    // Obtain the values from the text box for radius  id='radius'
-	radius = document.getElementById('radius').value;
-	
-	// Convert it to a number so we can do math against it
-	radius = parseFloat(radius);
-	
-	// Obtain the value from the text box for volume	id='height'
-	volume = document.getElementById('height').value;
-	
-	// Convert it to a number so we can do math against it
-	volume = parseFloat(volume);
-	
-	// Apply the formula    pi * radius^2 * volume
-	var  volumeOfCircle = pi * radius * radius * volume;
-	
-	// Update the onscreen area identified as id 'answer1'
-	var answerToUpdate = document.getElementById('answer1');
-	answerToUpdate.innerHTML = 'The volume of the circle is ' + volumeOfCircle.toLocaleString('en', {minimumFractionDigits: 3}); //result formatted as a string with representation of the number with 3 decimals points.
-}
+/*
+access to this content is restricted for privacy reasons 
+if you are a prospective employer and would like to see my source code,
+please contact me for more information. 
+*/
