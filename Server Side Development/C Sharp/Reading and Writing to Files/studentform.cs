6c2266640179438dc14c662b056e56515bada9b2
Alisher Yuldashev <alyuldashev@yahoo.ca>
2018-05-02 11:41:26 -0600

    Create studentform.cs

---
+++ Server Side Development/C Sharp/Reading and Writing to Files/studentform.cs
@@ -0,0 +1,21 @@
+/*
+C# Reading and writing files Lab #2 by Alisher Yuldashev. 
+Innotech College(Programming Diploma - SharePoint Specialization)
+
+This C# program Create a student application entry form and store the results in a file stream called “studentapplication.txt”.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studentform {
+
+ /*
+			access to this content is restricted for privacy reasons 
+			if you are a prospective employer and would like to see my source code,
+			please contact me for more information. 
+		*/
