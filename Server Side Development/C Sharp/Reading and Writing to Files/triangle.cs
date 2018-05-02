0ecb2a4d61d8d80deffdf5fd546f978d15618246
Alisher Yuldashev <alyuldashev@yahoo.ca>
2018-05-02 11:39:28 -0600

    Create triangle.cs

---
+++ Server Side Development/C Sharp/Reading and Writing to Files/triangle.cs
@@ -0,0 +1,24 @@
+/*
+C# Reading and writing files Lab #1 by Alisher Yuldashev. 
+Innotech College(Programming Diploma - SharePoint Specialization)
+
+This C# program displays a table that represents a triangle of any size. 
+In triangle, the first and the second rows are set to 1. Each element of 
+the triangle (from the third row downward) is the sum of the element directly 
+above it and the element to the left of the element directly above it. 
+And store the result in a “triangle.txt” file stream.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Triangle {
+ /*
+			access to this content is restricted for privacy reasons 
+			if you are a prospective employer and would like to see my source code,
+			please contact me for more information. 
+		*/
